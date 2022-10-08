886dee5412c802f822bdcc8430df23616d9d7113
github_andreglock <github_andreglock@gmail.com>
2022-10-08 20:15:46 +0200

    pull main

---
+++ src/app/feature-module/interfaces/case.interface.ts
@@ -0,0 +1,11 @@
+export interface ICase {
+  datum: Date,
+  volljarig: string,
+  geschlecht: string,
+  wohnsituation: string,
+  aufenthaltstatus: string,
+  krankenversicherung: string,
+  notvallv: string,
+  medvers: string,
+  fachbereich: string,
+}
