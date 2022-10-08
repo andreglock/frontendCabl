097cfd2c80e0b1e546005c2dc5696a42ebc1b63d
ludwigwermke <ludwig.wermke@posteo.de>
2022-10-08 22:26:01 +0200

    save work progress

--- src/app/feature-module/new-entry/new-entry.popup.component.ts
+++ src/app/feature-module/new-entry/new-entry.popup.component.ts
@@ -1,22 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-new-entry.popup',
   templateUrl: './new-entry.popup.component.html',
-  styleUrls: ['./new-entry.popup.component.scss']
+  styleUrls: ['./new-entry.popup.component.scss'],
 })
 export class NewEntryPopupComponent implements OnInit {
+  public optionAges = [
+    'Ja',
+    'Nein',
+    'Patient*in kann oder will keine Angabe machen',
+    'nicht gefragt/dokumentiert',
+  ];
 
-  constructor() { }
+  public optionSex = [
+    'mänlich',
+    'weiblich',
+    'inter/ divers',
+    'Patient*in kann oder will keine Angabe machen',
+    'nicht gefragt/dokumentiert',
+  ];
 
-  ngOnInit(): void {
-  }
+  constructor(
+    public dialogRef: MatDialogRef<NewEntryPopupComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: {}
+  ) {}
+
+  ngOnInit(): void {}
 
   cancel() {
-
+    this.dialogRef.close();
   }
 
   save() {
-
+    this.dialogRef.close();
   }
 }
