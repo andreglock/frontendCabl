3ce4c292c9986a7305eed4397d837ba0c94c386f
github_andreglock <github_andreglock@gmail.com>
2022-10-09 10:56:02 +0200

    added cors anywhere

---
+++ src/utils/server.js
@@ -0,0 +1,12 @@
+import cors_proxy from 'cors-anywhere';
+
+const port = 5000;
+const host = '127.0.0.1'
+
+cors_proxy.createServer({
+    originWhitelist: [], // Allow all origins
+    requireHeader: ['origin', 'x-requested-with'],
+    removeHeaders: ['cookie', 'cookie2']
+}).listen(port, host, function() {
+    console.log('Running CORS Anywhere on ' + host + ':' + port);
+});
